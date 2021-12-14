bef46be211aadd4ce62a297ca67198014f8cf34c
Jack Harrison <33330499+jackharrison27@users.noreply.github.com>
2021-12-14 17:42:25 +0000

    Updates outputs directory location

--- train.py
+++ train.py
@@ -72,8 +72,8 @@ def main():
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
     
-    os.makedirs('outputs', exists_ok=True)
-    joblib.dump(value=model, filename='outputs/hyperdrive_model.joblib')
+    os.makedirs('./outputs', exists_ok=True)
+    joblib.dump(value=model, filename='./outputs/hyperdrive_model.joblib')
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
